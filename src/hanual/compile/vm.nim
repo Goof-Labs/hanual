10edf1c71b4de975b65c22c8323e16a2f3637aed
Topscientist <74202229+Topscientist@users.noreply.github.com>
2023-04-23 12:38:44 +0100

    Added inc libraries into docs and created vm file

---
+++ src/hanual/compile/vm.nim
@@ -0,0 +1,8 @@
+# Nim VM - Nim is very similiar to python and compiles to C, so it's got a speedy runtime.
+# You may need to install the nim VS code xtension to see this in wonderful syntax highlighting
+
+# Parse file into various parts (Instruction, const, etc. pools)
+
+# Create area of memory (Maybe)
+
+# Create switch case statement (Massive lost of if, elifs and 1 else (Execption))

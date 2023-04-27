cfb944da3eb79e6fdd36ae7ee8941355a9b06f20
CookieScientist <98346353+CookieScientist@users.noreply.github.com>
2023-04-27 16:00:48 +0000

    I comitted a comment

--- src/hanual/vm/vm.nim
+++ src/hanual/vm/vm.nim
@@ -1,6 +1,7 @@
 # Nim VM - Nim is very similiar to python and compiles to C, so it's got a speedy runtime.
 # You may need to install the nim VS code extension to see this in wonderful syntax highlighting
 
+# Create struct - Wayt of organising data
 # Parse file into various parts (Instruction, const, etc. pools)
 
 # Create area of memory (Maybe)

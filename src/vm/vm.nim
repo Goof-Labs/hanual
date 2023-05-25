18ef28d07ea37f56ed307ff612101c37449fcd62
CookieScientist <98346353+CookieScientist@users.noreply.github.com>
2023-05-25 16:00:01 +0000

    Created the basis for pasing the file, added tbc links

--- src/vm/vm.nim
+++ src/vm/vm.nim
@@ -36,6 +36,13 @@ Nim VM, Virtual machine for compiling and interepreting the hanual bytecode. It 
 
         # File extension (Not related to chenobal)
         .chnl
+
+        # Docs links
+        https://nim-lang.org/docs/manual.html#exception-handling
+        https://nim-lang.org/docs/manual.html#statements-and-expressions-while-statement
+        https://nim-lang.org/docs/os.html
+        https://nim-lang.org/docs/tut2.html#exceptions-raise-statement
+        https://stackoverflow.com/questions/34427858/reading-bytes-from-many-files-performance
 ]#
 
 # Imports
@@ -49,7 +56,8 @@ var
 
 if open(f, "main.chnl"):
     while end != True:
-        ...
+        for instructions in f:
+            ...
 else:
     raise "The main file does not exist, please create one."    
 
